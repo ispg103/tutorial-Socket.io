a5640222ece04c55b7caf647af8974d26371fc06
Your Name <you@example.com>
2023-09-11 10:15:17 -0500

    commit

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
-  });
+});
 
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
